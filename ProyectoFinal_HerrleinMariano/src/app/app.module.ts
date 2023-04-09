ce371481b2d651411ca5ecdf623d35f7893d00d9
MarianoHerrlein <marianoherrlein@hotmail.com>
2023-04-08 21:32:39 -0300

    acerca de mi
    maqueta de acerca de mi

--- ProyectoFinal_HerrleinMariano/src/app/app.module.ts
+++ ProyectoFinal_HerrleinMariano/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { BannerComponent } from './components/banner/banner.component';
     AppComponent,
     HeaderComponent,
     BannerComponent,
+    AcercaDeComponent,
     
   ],
   imports: [
