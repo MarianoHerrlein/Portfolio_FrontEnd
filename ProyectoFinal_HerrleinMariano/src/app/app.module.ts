16002c9470bc20a50049dbc05ad3d553ff1bbea7
MarianoHerrlein <marianoherrlein@hotmail.com>
2023-04-08 21:43:57 -0300

    Sección educacion
    Maqueta de sección educación

--- ProyectoFinal_HerrleinMariano/src/app/app.module.ts
+++ ProyectoFinal_HerrleinMariano/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
+    EducacionComponent,
     
   ],
   imports: [
